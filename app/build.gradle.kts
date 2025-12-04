58e9d789e93f5125425647a96255790659e695b5
Michael (Micky) Lephend <idpwf1@gmail.com>
2025-12-04 09:09:52 -0600

    Added debuggable=false in buikd
    removed old wrong logo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,13 +17,14 @@ android {
         minSdk = 35
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
+            isDebuggable = false
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
